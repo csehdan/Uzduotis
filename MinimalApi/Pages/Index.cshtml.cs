6b41357fb26fde041fe55f6a9ea9ec924405b144
csehdan <csehdan@gmail.com>
2026-01-21 21:09:14 +0200

    typo

--- MinimalApi/Pages/Index.cshtml.cs
+++ MinimalApi/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MinimalFontEnd.Pages
+namespace MinimalFrontEnd.Pages
 {
 	public class IndexModel : PageModel
 	{
